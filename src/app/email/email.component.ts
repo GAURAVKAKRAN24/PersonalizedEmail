898797d2fbbecca2a509764170d3c311e3ea7385
GAURAVKAKRAN24 <gauravkakran24@gmail.com>
2025-05-25 19:49:32 +0530

    Updated

--- src/app/email/email.component.ts
+++ src/app/email/email.component.ts
@@ -23,7 +23,8 @@ import {
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatStepperModule } from '@angular/material/stepper';
 @Component({
   selector: 'app-email',
   templateUrl: './email.component.html',
@@ -40,19 +41,36 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatButtonModule,
     MatIconModule,
     MatProgressSpinnerModule,
+    MatCardModule,
+    MatStepperModule,
   ],
 })
 export class EmailComponent implements OnInit {
   emailForm: FormGroup = new FormGroup({});
+  configForm: FormGroup = new FormGroup({});
   selectedFile: File | null = null;
-  private _snackBar = inject(MatSnackBar);
+  private readonly _snackBar = inject(MatSnackBar);
   isLoading = false;
+  loaderMessage = '📩 Sending emails please wait';
+  sentSuccess = false;
   constructor(
     private readonly fb: FormBuilder,
     private readonly http: HttpClient
   ) {}
 
   ngOnInit(): void {
+    this.generateConfigForm();
+    this.generateSendMailForm();
+  }
+
+  generateConfigForm() {
+    this.configForm = this.fb.group({
+      senderGmail: ['gauravkakran24@gmail.com', [Validators.required, Validators.pattern(EMAIL_REGEX)]],
+      password: ['bdpsxfnvxeewivls', Validators.required],
+    });
+  }
+
+  generateSendMailForm() {
     this.emailForm = this.fb.group({
       companies: this.fb.array([
         this.fb.group({
@@ -96,6 +114,8 @@ export class EmailComponent implements OnInit {
     this.isLoading = true;
     console.log(this.emailForm.value);
     const formData = new FormData();
+    formData.append('senderGmail', this.configForm.value.senderGmail);
+    formData.append('password', this.configForm.value.password);
     formData.append('subject', this.emailForm.value.subject);
     formData.append('body', this.emailForm.value.body);
     formData.append(
@@ -109,19 +129,28 @@ export class EmailComponent implements OnInit {
     this.http.post(`${API_URL}send-emails`, formData).subscribe(
       (response: any) => {
         if (response) {
-          this.isLoading = false;
           this.openSnackBar(response.message, 'Ok');
+          this.loaderMessage = `📩 ${response.message}`;
           this.emailForm.reset();
+          this.mailSendSuccess();
         }
       },
       (error) => {
         console.error('Error:', error);
-        this.openSnackBar(error, 'Close');
+        this.openSnackBar(error.message, 'Close');
+        this.loaderMessage = `📩 ${error.message}`;
+        this.mailSendSuccess();
       }
     );
   }
 
   openSnackBar(message: string, action: string) {
     this._snackBar.open(message, action);
+  }
+
+  mailSendSuccess() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 3000);
   }
 }
