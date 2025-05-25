704f3b0ba1de248f6149499faa5e042738c45ff9
GAURAVKAKRAN24 <gauravkakran24@gmail.com>
2025-05-25 13:19:19 +0530

    Add more components

--- src/app/mobile-header/mobile-header.component.ts
+++ src/app/mobile-header/mobile-header.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-mobile-header',
   templateUrl: './mobile-header.component.html',
   styleUrls: ['./mobile-header.component.scss'],
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatMenuModule, MatButtonModule]
+  imports: [CommonModule, MatIconModule, MatMenuModule, MatButtonModule, RouterModule]
 })
 export class MobileHeaderComponent implements OnInit {
 
