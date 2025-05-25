c4a6b0645b0d02c7dcab863a1f4e0899d01b53e1
GAURAVKAKRAN24 <gauravkakran24@gmail.com>
2025-05-25 23:31:26 +0530

    Updated

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [RouterModule]
 })
 export class BannerComponent implements OnInit {
 
